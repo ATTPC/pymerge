1c55aa5ce57613da044e4dd363ebce27348e5e8e
Joshua Bradt <bradt@nscl.msu.edu>
2015-08-19 17:09:25 -0400

    Worked out packaging.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name = "pymerge",
+    version = "1.0",
+    scripts = ['pymerge'],
+
+    install_requires = ['numpy>=1.8',
+                        'clint>=0.4',
+                        'scipy>=0.16',
+                        'pytpc'],
+
+    author = 'Joshua Bradt',
+    author_email = 'bradt@nscl.msu.edu',
+    description = 'A package for merging GRAW data files',
+    keywords = 'graw data merge',
+    url = 'http://github.com/attpc/pymerge'
+)
