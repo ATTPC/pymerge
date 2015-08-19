945c7b437c92853df492c96aca7a91dc2d59fa1d
Joshua Bradt <bradt@nscl.msu.edu>
2015-08-19 13:59:33 -0400

    Added more logs, caught some errors.

--- pymerge.py
+++ pymerge.py
@@ -44,7 +44,7 @@ def subtract_and_delete_fpn(traces):
                                     traces['aget'] == aget, ~np.in1d(traces['channel'], fpn_channels)), axis=0))[0]
 
         if len(fpn_idx) != 4:
-            warnings.warn('Number of FPN channels was incorrect: %d (should be 4)' % len(fpn_idx))
+            logger.warn('Number of FPN channels was incorrect: %d (should be 4)' % len(fpn_idx))
 
         mean_fpn = traces['data'][fpn_idx].mean(axis=0)
         mean_fpn -= mean_fpn.mean()
@@ -63,13 +63,22 @@ class EventProcessor(mp.Process):
         self.outq = outq
         super().__init__()
 
-    def process_event(self, evtid, timestamp, frames):
+    def process_event(self, evtid, timestamp, rawframes):
+        frames = [pytpc.grawdata.GRAWFile._parse(r) for r in rawframes]
         evt = pytpc.Event(evtid, timestamp)
         evt.traces = np.concatenate(frames)
 
         evt.traces['pad'] = [self.lookup.get(tuple(a), 20000) for a in evt.traces[['cobo', 'asad', 'aget', 'channel']]]
 
         evt.traces = subtract_and_delete_fpn(evt.traces)
+
+        bad_channels = np.where(evt.traces['pad'] == 20000)[0]
+        if len(bad_channels) != 0:
+            addr_list = '[' + ', '.join(('{}/{}/{}/{}'.format(evt.traces['cobo'][i], evt.traces['asad'][i],
+                                                              evt.traces['aget'][i], evt.traces['channel'][i])
+                                                              for i in bad_channels)) + ']'
+            logger.warn('Event %d contained unmapped channels: %s', evtid, addr_list)
+            evt.traces = np.delete(evt.traces, bad_channels)
 
         if self.peds is not None:
             evt.traces['data'] = (evt.traces['data'].T - self.peds[evt.traces['pad']]).T
@@ -82,7 +91,9 @@ class EventProcessor(mp.Process):
     def run(self):
         while True:
             try:
+                logger.debug('Getting input from inq')
                 inp = self.inq.get()
+                logger.debug('Got input from inq')
                 if isinstance(inp, str) and inp == 'STOP':
                     logger.debug('Worker received STOP')
                     break
@@ -90,16 +101,24 @@ class EventProcessor(mp.Process):
                     evtid, timestamp, frames = inp
             except TypeError:
                 logger.exception('TypeError in worker when receiving frames')
+            except (KeyboardInterrupt, SystemExit):
+                logger.debug('Got interrupted. Dying.')
+                return
             finally:
                 self.inq.task_done()
 
             try:
                 evt = self.process_event(evtid, timestamp, frames)
                 logger.debug('Processed event %d', evtid)
+            except (KeyboardInterrupt, SystemExit):
+                logger.debug('Got interrupted. Dying.')
+                return
             except:
                 logger.exception('Exception in event processing')
             else:
+                logger.debug('Waiting to put event on outq')
                 self.outq.put(evt)
+                logger.debug('Finished putting event on outq')
 
 
 class WriterProcess(mp.Process):
@@ -113,21 +132,27 @@ class WriterProcess(mp.Process):
         num_events_processed = 0
         while True:
             try:
-                # logger.debug('Getting output')
+                logger.debug('Getting output')
                 r = self.outq.get()
+                logger.debug('Got output')
                 if isinstance(r, str) and r == 'STOP':
                     logger.debug('Writer received stop')
                     break
                 else:
                     outfile.write(r)
                     logger.debug('Wrote event %d', r.evt_id)
+            except (KeyboardInterrupt, SystemExit):
+                logger.debug('Got interrupted. Dying.')
+                return
             finally:
                 self.outq.task_done()
                 num_events_processed += 1
         outfile.close()
 
 def main():
+
     # Parse the command line options
+
     parser = argparse.ArgumentParser(description='GRAW Frame merger')
     parser.add_argument('input', type=str,
                         help='Path to a directory of GRAW files')
@@ -141,9 +166,19 @@ def main():
                         help='Threshold value')
     parser.add_argument('--zerosupp', '-z', action='store_true',
                         help='Enable zero suppression in the output file')
+    parser.add_argument('--verbose', '-v', action='count', default=0,
+                        help='Print more information')
     args = parser.parse_args()
 
-    # Find the GRAW files
+    if args.verbose == 1:
+        logger.setLevel(logging.INFO)
+    elif args.verbose >= 2:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.WARN)
+
+    # Find and map the GRAW files
+
     print('Looking for files')
     gfile_paths = glob.glob(os.path.join(os.path.abspath(args.input), '*.graw'))
     with indent(4):
@@ -156,6 +191,8 @@ def main():
     for path in progress.bar(gfile_paths):
         gf = pytpc.grawdata.GRAWFile(path)
         gf.close()
+
+    # Load the lookup table and pedestals, and parse the threshold option
 
     if args.lookup is not None:
         print('Loading lookup table')
@@ -184,15 +221,20 @@ def main():
     else:
         threshold = None
 
+    # Now open the files for reading
+
     gfiles = [pytpc.grawdata.GRAWFile(g) for g in gfile_paths]
-    valid_events = sorted(reduce(set.intersection, [set(g.evtids) for g in gfiles]))
+    valid_events = sorted(reduce(set.union, [set(g.evtids) for g in gfiles]))  # TODO: Make this check for frames that are in all cobos
+
+    logger.debug('Length of set of valid events was %d', len(valid_events))
 
     if args.output is not None:
         output_name = args.output
     else:
         run_name = os.path.split(os.path.normpath(args.input))[-1]
         output_name = run_name + '.evt'
-    # outfile = pytpc.evtdata.EventFile(output_name, 'w')
+
+    # Prepare the queues and worker/writer processes
 
     logger.debug('Making queues')
 
@@ -201,33 +243,39 @@ def main():
 
     logger.debug('Making processes')
 
-    workers = [EventProcessor(lookup, peds, threshold, inq, outq) for i in range(4)]
+    workers = [EventProcessor(lookup, peds, threshold, inq, outq) for i in range(8)]
     for w in workers:
         w.start()
     writer = WriterProcess(outq, output_name)
     writer.start()
 
+    # Now start putting frames on the input queue
+
+    print('Beginning merge')
+
     for i in progress.bar(valid_events):
         frames = []
         for g in gfiles:
-            this_frames = list(g.get_frames_for_event(i))
+            this_frames = g.get_raw_frames_for_event(i)
             frames += this_frames
 
         if len(frames) == 0:
             logger.warn('No frames in event %d', i)
             continue
-        # f, h = gfiles[0].get_frames_for_event(i)[0]
-        # timestamp = h['timestamp']
+        else:
+            logging.debug('Found %d frames for event %d', len(frames), i)
+
         inq.put((i, 0, frames))
-        # evt = proc.process_event(i, 0, frames)
-        # outfile.write(evt)
+        logger.debug('Put in frames for event %d', i)
+
+    # Wrap up the queues and worker/writer processes
 
     logger.debug('Waiting for all jobs to finish')
-    inq.join()
+    inq.join()  # blocks main thread until queue is empty and all tasks on it are finished
     outq.join()
 
-    for i in range(4):
-        inq.put('STOP')
+    for i in range(len(workers)):
+        inq.put('STOP')  # This tells the processes to die
     outq.put('STOP')
 
     for proc in workers:
@@ -237,5 +285,4 @@ def main():
 
 if __name__ == '__main__':
     logger = mp.log_to_stderr()
-    logger.setLevel(logging.WARN)
     main()
