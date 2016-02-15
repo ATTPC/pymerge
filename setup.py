8e942a395c1e350e1673d692561a2989b1bf3389
Joshua Bradt <bradt@nscl.msu.edu>
2016-02-15 11:34:27 -0500

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name="pymerge",
-    version="1.2",
+    version="1.3",
     scripts=['pymerge', 'peaks2hdf', 'evt2hdf'],
     install_requires=['numpy>=1.9',
                       'clint>=0.4',
                       'scipy>=0.16',
                       'h5py>=2.5',
-                      'pytpc'],
+                      'pytpc>=0.7'],
     author='Joshua Bradt',
     author_email='bradt@nscl.msu.edu',
     description='A package for merging and processing GRAW data files',
